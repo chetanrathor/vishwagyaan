9eda313b0d8ec02c13a87d4c75ab1c18db336e59
Chetan Rathore <crathore@grepruby.io>
2024-12-17 01:05:12 +0530

    ADDED THE AD SENSE CODE TAG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9140105035431584"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
